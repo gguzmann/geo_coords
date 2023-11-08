059a28bde8a017173f2a6db87e215edc2dc02de6
gabusho92 <g.guzman.nunez@gmail.com>
2023-11-08 13:46:54 -0300

    export module

--- directions.js
+++ directions.js
@@ -1,5 +1,7 @@
-export const directions = [
+const directions = [
     'Plaza Baquedano',
     'Pío Nono 150, Recoleta, Región Metropolitana',
     'San Pablo 6750'
 ]
+
+module.exports = {directions}
--- index.js
+++ index.js
@@ -2,14 +2,22 @@ const puppeteer = require('puppeteer');
 const { directions } = require('./directions');
 
 const geo = async () => {
+    console.log('Abriendo navegador')
     const browser = await puppeteer.launch({ headless: true});
+
     for (const dir of directions) {
+        console.log('nueva pagina')
         const page = await browser.newPage();
+
+        console.log('ingresar a google map')
         await page.goto('https://www.google.cl/maps/preview');
         const input = await page.$('#searchboxinput')
-        await input.type(dir)
 
+        console.log('tipear direccion', dir)
+        await input.type(dir)
         await page.click('#searchbox-searchbutton');
+        
+        console.log('Esperar 5 seg')
         await page.waitForTimeout(5000);
         const url = await page.url().split('@');
         const coord = {
